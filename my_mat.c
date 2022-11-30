86f66a0e90f72a739a74df6617df189219da525f
root <root@RONPC.localdomain>
2022-11-30 12:06:37 +0200

    WIP

--- my_mat.c
+++ my_mat.c
@@ -2,7 +2,9 @@
 #include <math.h>
 #include <limits.h>
 int matrix [10][10];
-
+int matrix1 [10][10];
+int max = INT_MIN;
+int max1 = INT_MIN;
 int min(int a , int b){
     return (a > b) ? b : a ;
 }
@@ -15,18 +17,42 @@ for(int i = 0; i < 10; i++)
         for(int j = 0; j < 10; j++)
         {
             scanf("%3d", & matrix[i][j]);
+            if (max < matrix[i][j])
+            {
+                max = matrix[i][j];
+            }
+            
         }
-    }  
+    }
+    for (int k = 0; k < 10; k++)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+
+                    matrix1[i][j] = min(matrix[i][j] , matrix[i][k] + matrix[k][j]);
+
+                    if (max1 < matrix1[i][j])
+                    {
+                        max1 = matrix1[i][j];
+                    }
+
+                }
+            }
+        } 
+    max = max + max1 +1;    
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
         {
             if (matrix[i][j] == 0)
             {
-                matrix[i][j] = INT_MAX;
+                matrix[i][j] = max;
             }
         }
     }
+
 
     // for(int i = 0; i < 3; i++)
     // {
@@ -49,17 +75,22 @@ void matTrue(){
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    if (matrix[i][j] != INT_MAX)
-                    {
+
                         matrix[i][j] = min(matrix[i][j] , matrix[i][k] + matrix[k][j]);
-                    }
+
+ 
                 }
             }
         }      
-    if ( matrix[numI][numJ] == INT_MAX)
+    if ( matrix[numI][numJ] == max)
         {
         printf("False\n");
         }
+    else if (numI == numJ)
+    {
+        printf("False\n");
+    }
+        
     else
         {
         printf("True\n");
@@ -72,10 +103,15 @@ void matPath(){
     int numI , numJ ;
     scanf("%d%d" , & numI , &numJ); 
 
-    if (matrix[numI][numJ] == INT_MAX)
+    if (matrix[numI][numJ] == max)
     {
         printf("%d\n" , -1);
     }
+    else if (numI == numJ)
+    {
+         printf("%d\n" , -1);
+    }
+    
     else {
     for (int k = 0; k < 10; k++)
         {
@@ -83,7 +119,7 @@ void matPath(){
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    if (matrix[i][j] != INT_MAX)
+                    if (matrix[i][j] != max)
                     {
                         matrix[i][j] = min(matrix[i][j] , matrix[i][k] + matrix[k][j]);
                     }
